d66a8136658522970e2b2e6a4ed0b1fa11f69652
malsimity <maksim-shirokov-01@mail.ru>
2022-12-13 16:53:49 +0300

    new events

---
+++ FileEDF.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using EDFCSharp;
+using Microsoft.Win32;
+
+namespace ECGTools
+{
+    static public class FileEDF
+    {
+        static public EDFSignal ImportEdfSignal()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "EDF-файл (*.edf)|*.edf" };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                return new EDFFile(openFileDialog.FileName).Signals[0];
+            }
+            else
+            {
+                MessageBox.Show("Ошибка считывания файла!");
+                return null;
+            }
+        }
+
+        static public void ImportDataAnaliz()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "analiz-файл (*.analiz)|*.analiz" };
+
+            // Логика считывания файлов анализа
+        }
+
+        static public void ExportEdfSignal(int[] peakR, int[] classes)
+        {
+            if (peakR == null || classes == null) { throw new ArgumentNullException("Записывать нечего"); }
+
+            // Далее логика записи номеров отчётов, где зафиксированы R-пики и номера классов, к которым принадлежат эти R-пики
+
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,25 +26,93 @@ namespace ECGTools
         public MainWindow()
         {
             InitializeComponent();
-            // Считывание и преобразование данных
-            string filleName = @"D:\Neurosoft\MyPROJECT\DataSet\Тестовые записи Accordix\ecg_record (24).edf";
-            double constant = 0; // берём первый элемент массива из матлаба
-            var edfFile = new EDFFile(filleName);
-            int freq = (int)edfFile.Signals[0].FrequencyInHZ;
-            double[] signal = new double[edfFile.Signals[0].SamplesCount];
-            signal[0] = edfFile.Signals[0].ScaledSample(0) - constant;
-            double[] x = new double[signal.Length];
-            for (int i = 1; i < signal.Length; i++)
+        }
+
+        private EDFSignal signal = null;
+        private double[] filtSig = null;
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9,]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        public void PlotSig(EDFSignal signal)
+        {
+            float[] sig = new float[signal.SamplesCount];
+            float[] x = new float[sig.Length];
+            int freq = (int)signal.FrequencyInHZ;
+            for (int i = 0; i < sig.Length; i++)
             {
-                signal[i] = (float)edfFile.Signals[0].ScaledSample(i);
-                signal[i] -= Math.Abs(signal[0]);
-                x[i] = i / (double)freq;
+                sig[i] = (float)signal.ScaledSample(i);
+                sig[i] -= Math.Abs(sig[0]);
+                x[i] = i / (float)freq;
             }
-            signal[0] = constant;
-            //x = x.Take(10000).ToArray();
-            //signal = signal.Take(10000).ToArray();
+
+            Plot.Plot.AddSignalXYConst(x, sig);
+            Plot.Refresh();
+        }
+
+        public void PlotSig(double[] signal, int freq)
+        {
+            float[] x = new float[signal.Length];
+            for (int i = 0; i < signal.Length; i++)
+                x[i] = i / (float) freq;
+
             Plot.Plot.AddSignalXYConst(x, signal);
             Plot.Refresh();
+        }
+
+        private void Menu_Import_Open_OnClick(object sender, RoutedEventArgs e)
+        {
+            signal = FileEDF.ImportEdfSignal();
+            PlotSig(signal);
+        }
+
+        private void CbFilt_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (signal == null)
+            {
+                MessageBox.Show("Ипортируйте данные!");
+                return;
+            }
+
+            int freq = (int)signal.FrequencyInHZ;
+            int pow = 0;
+            while (Math.Pow(2, pow) < signal.SamplesCount)
+            {
+                pow++;
+            }
+
+            double[] sigBuf = new double[(int)Math.Pow(2, pow)];
+            for (int i = 0; i < signal.SamplesCount; i++)
+                sigBuf[i] = signal.ScaledSample(i);
+            for (int i = (int)signal.SamplesCount; i < (int)Math.Pow(2, pow); i++)
+                sigBuf[i] = 0;
+            
+            sigBuf = FftSharp.Filter.BandPass(sigBuf, freq, Convert.ToDouble(tbLowfreq.Text), Convert.ToDouble(tbHightfreq.Text));
+            filtSig = new double[signal.SamplesCount];
+            for (int i = 0; i < signal.SamplesCount; i++)
+                filtSig[i] = sigBuf[i];
+
+            Plot.Plot.Clear();
+            PlotSig(filtSig, freq);
+            tbLowfreq.IsEnabled = false;
+            tbHightfreq.IsEnabled = false;
+        }
+
+        private void CbFilt_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            if (signal == null)
+            {
+                MessageBox.Show("Ипортируйте данные!");
+                return;
+            }
+
+            Plot.Plot.Clear();
+            PlotSig(signal);
+            tbLowfreq.IsEnabled = true;
+            tbHightfreq.IsEnabled = true;
         }
     }
 }
